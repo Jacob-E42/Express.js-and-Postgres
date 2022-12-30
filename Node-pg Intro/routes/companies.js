e5ef6a0ff3b96c213a90e74c1881561d0661aa3d
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-30 10:52:55 -0500

    added GET route for a single company

--- Node-pg Intro/routes/companies.js
+++ Node-pg Intro/routes/companies.js
@@ -5,7 +5,17 @@ const router = express.Router();
 router.get("/", async function (req, res, next) {
 	try {
 		const results = await db.query(`SELECT code, name FROM companies;`);
-		return res.json(results.rows);
+		return res.json({ companies: results.rows });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.get("/:code", async (req, res, next) => {
+	try {
+		const result = await db.query(`SELECT * FROM companies WHERE code=$1`, [req.params.code]);
+		if (result.rowCount === 0) return next();
+		return res.json({ company: result.rows[0] });
 	} catch (err) {
 		return next(err);
 	}
